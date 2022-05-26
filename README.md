e3abc540c81b7ece6e53a2cb61d3589b4402f85c
Siraj Ur Rahman <siraj.urrahman@n3o.ltd>
2022-05-26 19:16:17 +0500

    Add a code to run a javascript file whenever the file is cchanged.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # watchit
+This application will run whenever you change the code on the test js it will execute it automatically. 
+
+Command To Run:
+node index.js test.js
--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 #!/usr/bin/env node
-const debounce = require('lodash.debounce');
-const chokidar = require('chokidar');
-const program = require('caporal');
-const fs = require('fs');
 
-program.version('0.0.1')
-.argument('[filename]', 'Name of a file to execute')
-.action(async({filename})=> {
+const debounce = require("lodash.debounce");
+const chokidar = require("chokidar");
+const program = require("caporal");
+const fs = require("fs");
+const { spawn } = require("child_process");
 
-    const name = filename || 'index.js';
+program
+  .version("0.0.1")
+  .argument("[filename]", "Name of a file to execute")
+  .action(async ({ filename }) => {
+    const name = filename || "index.js";
     try {
-        await fs.promises.access(name);
-    } catch(err) {
-        throw new Error(`Couldn't not find the file ${name}`);
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Couldn't not find the file ${name}`);
     }
-});
+    const start = debounce(() => {
+      return spawn("node", [name], { stdio: "inherit" });
+    }, 250);
+
+    chokidar
+      .watch(".")
+      .on("add", start)
+      .on("change", start)
+      .on("unlink", start);
+  });
 
 program.parse(process.argv);
-
-const start = debounce(()=>{
-    console.log('Starting User program')
-}, 100);
-
-
-chokidar.watch('.')
-.on('add', start)
-.on('change', () => console.log('file changed'))
-.on('unlink', () => console.log('File Unlinked'));
